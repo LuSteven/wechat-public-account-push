f401350164ac7051b5195639d490a4683152cc46
陆丹雍 <627573361@qq.com>
2022-11-15 15:07:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,44 +7,45 @@
  */
 const USER_CONFIG = {
 
+  USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx12a0eb2f9711ccc2',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'bedbf9fce3cfaf141aa1e3fc5aade75a',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '浙江',
+  CITY: '杭州',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '菲菲仔',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'd831e852cde74810875ac1d468335a5d',
+      id: 'PDU17892TMmmSvpPWrVbtHWI1YTP7XpWg7VBo1NHb',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '48bsFibKgqL-H_rJw0q1Yjkkdix5FD3CE37_xf-1IAU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '06-06',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '1999', date: '04-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝贝', year: '1999', date: '06-06',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2022', date: '08-21',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-08-21' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
@@ -57,7 +58,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oRQRw584zPNEmdvS4Rahy2_TVCBE',
     }
   ],
 
